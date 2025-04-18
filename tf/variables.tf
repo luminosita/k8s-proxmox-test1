e9b641554ffe6ba2f42b45d8dc51a120aed1b9ec
luminosita <milosh@emisia.net>
2025-04-19 01:09:29 +0200

    network

--- tf/variables.tf
+++ tf/variables.tf
@@ -60,21 +60,24 @@ variable "talos_cluster_config" {
 variable "talos_nodes" {
   description = "Configuration for cluster nodes"
   type = map(object({
-    host_node      = string
-    machine_type   = string
-    datastore_id   = string
-    ip             = optional(string)
-    dns            = optional(list(string))
-    mac_address    = string
-    gateway        = optional(string)
-    subnet_mask    = optional(string)
-    network_device = optional(string)
-    vlan_id        = optional(number)
-    vm_id          = number
-    cpu            = number
-    ram_dedicated  = number
-    update         = optional(bool, false)
-    igpu           = optional(bool, false)
+    host_node    = string
+    machine_type = string
+    datastore_id = string
+    network = object({
+      dhcp        = bool
+      ip          = optional(string)
+      dns         = optional(list(string))
+      mac_address = string
+      gateway     = optional(string)
+      subnet_mask = optional(string, "24")
+      device      = optional(string)
+      vlan_id     = optional(number)
+    })
+    vm_id         = number
+    cpu           = number
+    ram_dedicated = number
+    update        = optional(bool, false)
+    igpu          = optional(bool, false)
     })
   )
 }
